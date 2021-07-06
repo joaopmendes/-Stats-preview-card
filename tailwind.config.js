6bc37049658028bde7c1fb5e408513f634180f8d
joaopmendes <joaopedrolopesmendes@gmail.com>
2021-07-05 17:41:18 -0700

    !FIX removed comments

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    fontFamily: {
+      'display': ['Inter', 'ui-monospace'],
+      'body': ['Lexend Deca', 'ui-monospace'],
+     },
+    colors: {
+      mainBackground: '#090b1a',
+      cardBackground: '#1b1938',
+      accent: '#aa5cdb',
+      mainWhite: '#ffffff',
+      mainParagraph: 'hsla(0, 0%, 100%, 0.75)',
+      statHeading: 'hsla(0, 0%, 100%, 0.6)'
+    }
+  },
+  variants: {
+    extend: {
+
+    },
+  },
+  plugins: [],
+}
